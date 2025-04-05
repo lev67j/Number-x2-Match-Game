e1b25a94f94b7ebfc49730a69851b22314688bdd
Lev <153025879+lev67j@users.noreply.github.com>
2025-04-05 17:23:26 +0300

    base version

---
+++ GameViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GameViewController.swift
+//  Number x2 Match Game
+//
+//  Created by Lev Vlasov on 2025-04-03.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as? SKView {
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
